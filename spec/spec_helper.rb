34ac5aca4b8d55d5ff3c916ba631ae51f2da46fe
Dylan <dylan.blakemore@zappistore.com>
2020-09-25 14:58:19 +0200

    Add coverage limit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
 require 'bundler/setup'
 require 'pry'
 require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/spec/"
+  minimum_coverage 99.5
+  maximum_coverage_drop 0.5
+end
+
 require 'structish'
 
 Bundler.setup
-SimpleCov.start
